d622f0f0d129b70407c2a3cf2a495f9f23f55c0e
sireeshasadineni <41733085+sireeshasadineni@users.noreply.github.com>
2018-07-26 20:00:22 -1200

    Create year.java

---
+++ year.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int a;
+	  Scanner obj=new Scanner(System.in);
+	  a=obj.nextInt();
+	  if(a%4==0)
+	  {
+	  	System.out.println("yes");
+	  }
+	  else
+	  {
+	  	System.out.println("no");
+	  }
+	}
+ }
